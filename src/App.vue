f0a6a61cb15175b83d0871e444e4affd0f6af091
shantespeaces <shantespeaces@gmail.com>
2024-05-17 11:10:01 -0400

    added google event for button clicks

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,26 @@
 </template>
 <script setup>
 import { RouterView } from "vue-router";
+import { onMounted } from "vue";
+
+const addEventListeners = () => {
+  const buttons = document.querySelectorAll("button");
+  buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      const label = event.target.innerText || event.target.value || "button";
+      if (typeof gtag !== "undefined") {
+        gtag("event", "click", {
+          event_category: "button",
+          event_label: label,
+        });
+      }
+    });
+  });
+};
+
+onMounted(() => {
+  addEventListeners();
+});
 </script>
 
 <style scoped></style>
